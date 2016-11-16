0d02c55b46f22a2ff6a6a6027510ce5dad920409
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-11-16 17:23:48 +1100

    Starting JSX/React work

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,50 @@
 'use strict';
 
 const webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     context: __dirname + "/src",
     entry: {
-        app: ["./index.js"],
+        app: [
+            "./index.js",
+        ],
     },
     output: {
         path: __dirname + "/dist",
         filename: "[name].bundle.js",
         publicPath: "/assets",
     },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ],
+        rules: [
+            {
+                test: /[.]scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+    ],
     devServer: {
         contentBase: __dirname + "/src",
-    }
+    },
 };
